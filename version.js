f4e614cb715c3f81234d1c98ce0bc4cd935f2d2e
Winterkuersche <ladwigmueller@web.de>
2026-03-09 21:02:23 +0100

    Update version.js

--- version.js
+++ version.js
@@ -1,5 +1,6 @@
 const APP_META = {
   name: "Wochenplan",
-  version: "V9.1",
-  cacheName: "wochenplan-cache-v42"
+  version: "V9.2",
+  cacheName: "wochenplan-cache-v43",
+  stateKey: "schleswig-holstein"
 };
