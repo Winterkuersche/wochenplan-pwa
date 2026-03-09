921ad5ac7e2f5f56efbbc69860d17568d9bdadd8
Winterkuersche <ladwigmueller@web.de>
2026-03-09 21:09:22 +0100

    Create shift-utils.js

---
+++ shift-utils.js
@@ -0,0 +1,133 @@
+function findEarlyShiftByCode(code) {
+  return SHIFT_CONFIG.earlyShifts.find((shift) => shift.code === code) || null;
+}
+
+function getLateShiftCodeFromStart(startHHMM) {
+  const startMinutes = hhmmToMinutes(startHHMM);
+  const endMinutes = hhmmToMinutes(SHIFT_CONFIG.lateShift.endWithoutCheckout);
+  const workedMinutes = endMinutes - startMinutes;
+  const workedHours = Math.round(workedMinutes / 60);
+
+  return `L${workedHours}`;
+}
+
+function buildEarlyShiftEntry(code) {
+  const shift = findEarlyShiftByCode(code);
+  if (!shift) return null;
+
+  const workedMinutes = getWorkedMinutesFromRange(
+    shift.start,
+    shift.end,
+    shift.breakMinutes
+  );
+
+  return {
+    type: "shift",
+    mode: "early",
+    code: shift.code,
+    label: shift.label,
+    start: shift.start,
+    end: shift.end,
+    breakMinutes: shift.breakMinutes,
+    minutes: workedMinutes
+  };
+}
+
+function buildLateShiftEntry(startHHMM, withCheckout) {
+  if (!SHIFT_CONFIG.lateShift.possibleStarts.includes(startHHMM)) {
+    return null;
+  }
+
+  const endHHMM = withCheckout
+    ? SHIFT_CONFIG.lateShift.endWithCheckout
+    : SHIFT_CONFIG.lateShift.endWithoutCheckout;
+
+  const breakMinutes = withCheckout
+    ? SHIFT_CONFIG.lateShift.extraBreakMinutesWithCheckout
+    : 0;
+
+  const workedMinutes = getWorkedMinutesFromRange(startHHMM, endHHMM, breakMinutes);
+  const code = getLateShiftCodeFromStart(startHHMM);
+
+  return {
+    type: "shift",
+    mode: "late",
+    code,
+    label: code,
+    start: startHHMM,
+    end: endHHMM,
+    breakMinutes,
+    withCheckout: !!withCheckout,
+    minutes: workedMinutes
+  };
+}
+
+function buildFullShiftEntry(withCheckout) {
+  const startHHMM = SHIFT_CONFIG.fullShift.start;
+  const endHHMM = withCheckout
+    ? SHIFT_CONFIG.fullShift.endWithCheckout
+    : SHIFT_CONFIG.fullShift.endWithoutCheckout;
+
+  const breakMinutes = SHIFT_CONFIG.fullShift.baseBreakMinutes +
+    (withCheckout ? SHIFT_CONFIG.fullShift.extraBreakMinutesWithCheckout : 0);
+
+  const workedMinutes = getWorkedMinutesFromRange(startHHMM, endHHMM, breakMinutes);
+
+  return {
+    type: "shift",
+    mode: "full",
+    code: "G",
+    label: "G",
+    start: startHHMM,
+    end: endHHMM,
+    breakMinutes,
+    withCheckout: !!withCheckout,
+    minutes: workedMinutes
+  };
+}
+
+function buildFlexibleShiftEntry(startHHMM, endHHMM) {
+  if (!isValidHHMM(startHHMM) || !isValidHHMM(endHHMM)) {
+    return null;
+  }
+
+  const totalSpanMinutes = diffMinutesBetweenHHMM(startHHMM, endHHMM);
+  if (totalSpanMinutes <= 0) return null;
+
+  const breakMinutes = getBreakMinutesForFlexibleShift(startHHMM, endHHMM);
+  const workedMinutes = getWorkedMinutesFromRange(startHHMM, endHHMM, breakMinutes);
+
+  if (workedMinutes < SHIFT_CONFIG.minWorkMinutes) {
+    return null;
+  }
+
+  return {
+    type: "shift",
+    mode: "flex",
+    code: "FLEX",
+    label: `${startHHMM}-${endHHMM}`,
+    start: startHHMM,
+    end: endHHMM,
+    breakMinutes,
+    minutes: workedMinutes
+  };
+}
+
+function isShiftEntry(value) {
+  return !!value && value.type === "shift";
+}
+
+function getShiftDisplayLabel(entry) {
+  if (!isShiftEntry(entry)) return "";
+
+  if (entry.mode === "flex") {
+    return entry.label || `${entry.start}-${entry.end}`;
+  }
+
+  return entry.label || entry.code || "";
+}
+
+function getShiftWorkedHHMM(entry) {
+  if (!isShiftEntry(entry)) return "00:00";
+  return minutesToHHMM(entry.minutes || 0);
+}
