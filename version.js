df17f1fb04d9610145b9f81ba06bd5b7ea6073a8
Winterkuersche <ladwigmueller@web.de>
2026-09-11 07:19:15 +0200

    Fix MEP PDF delivery on iOS

--- version.js
+++ version.js
@@ -1,7 +1,7 @@
 const APP_META = {
   name: "Wochenplan",
-  version: "V9.4",
-  assetVersion: "2026-09-10-2",
-  cacheName: "wochenplan-cache-v50",
+  version: "V9.5",
+  assetVersion: "2026-09-11-1",
+  cacheName: "wochenplan-cache-v51",
   stateKey: "schleswig-holstein"
 };
